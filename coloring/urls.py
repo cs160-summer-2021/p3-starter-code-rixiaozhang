5ebfa5605d56be9f4d894df36091a21f82b2f119
rixiaozhang <rixiaozhang@berkeley.com>
2021-07-15 18:28:02 +0800

    color picker feature

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('demo/', views.demo, name='demo'),
-     path('gallery/', views.gallery, name='gallery'),
+    path('gallery/', views.gallery, name='gallery'),
     path('new_interaction/', views.new_interaction, name='new_interaction')
 
 ]
